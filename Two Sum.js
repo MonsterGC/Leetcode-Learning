887f4ac51ff495ba15747e01b1b62e8edccef3f2
GCMonster <1724264854@qq.com>
2020-02-14 13:40:16 +0800

    Two Sum

---
+++ Two Sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+     var temp = [];
+    for(var i=0;i<nums.length;i++){
+        var dif = target - nums[i];
+        if(temp[dif] != undefined){
+            return [temp[dif],i];
+        }
+        temp[nums[i]] = i;
+    }
+};
