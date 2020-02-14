c78fe12259bcbdf75063eed4e39e159c838b3578
GCMonster <1724264854@qq.com>
2020-02-14 14:49:26 +0800

    Update Two Sum(CPP)

--- C++/Two Sum.cpp
+++ C++/Two Sum.cpp
@@ -2,6 +2,21 @@
 #include <unordered_map>
 using namespace std;
 
+
+/**
+ * 
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
 class Solution
 {
 public:
@@ -10,7 +25,7 @@ public:
         vector<int> result;
         unordered_map<int, int> hash;
         int temp;
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size(); i++)   
         {
             if (nums.begin() != nums.end())
             {
